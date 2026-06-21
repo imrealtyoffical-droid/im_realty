695ccc51dec4e4a2eaf934a9e71c5b133d15d6f2
imrealtyoffical-droid <imrealtyoffical@gmail.com>
2026-06-21 12:39:54 +0530

    Update FirstHomeVideo.tsx

--- src/components/home/FirstHomeVideo.tsx
+++ src/components/home/FirstHomeVideo.tsx
@@ -36,7 +36,7 @@ export default function FirstHomeVideo() {
     playsInline
     className="w-full"
   >
-    <source src="/images/web_video.mp4" type="video/mp4" />
+    <source src="/images/fhome.mp4" type="video/mp4" />
   </video>
 </div>
 
